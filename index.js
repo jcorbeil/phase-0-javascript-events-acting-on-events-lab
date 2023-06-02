62662c3831457009f87a0ae69bed77e1c0e39b0f
Jeremy Corbeil <jeremycorbeil9@yahoo.com>
2023-06-02 14:41:51 -0400

    finished-javascript-events-acting-on-events-lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+// Get the DODGER element
+const dodger = document.getElementById('dodger');
+
+// Function to move the DODGER to the left
+function moveDodgerLeft() {
+  const currentPosition = parseInt(dodger.style.left);
+  const newPosition = currentPosition - 10;
+  
+  if (newPosition >= 0) {
+    dodger.style.left = newPosition + 'px';
+  }
+}
+
+//Function to move the DODGER to the right
+function moveDodgerRight() {
+    const currentPosition = parseInt(dodger.style.left);
+    const newPosition = currentPosition + 10;
+    
+    const maxWidth = document.getElementById('game').offsetWidth - dodger.offsetWidth;
+    if (newPosition <=360) {
+      dodger.style.left = newPosition + 'px';
+    }
+  }
+
+  
